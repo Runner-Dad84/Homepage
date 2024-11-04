cc1c3f923e651acb7bda922298fb89663f7219b4
Runner-Dad84 <vanhoogie@gmail.com>
2024-11-03 20:34:31 -0500

    Event listener for click - prevent default

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ let navBtns = document.querySelectorAll('.navButn');
 
 console.log(navBtns);
 
+navBtns.forEach(btn => {
+    btn.addEventListener('click', (e)=> {
+        e.preventDefault();
+    })
+
+}
+)
+
